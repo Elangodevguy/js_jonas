00be32a0f3f9b550874b6e485b62996c8732c0d8
Elango <elango@Elangos-MacBook-Pro.local>
2021-03-04 20:34:08 +0530

    values_and_varibles

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,15 @@
 let js = "amazing";
-if (js === "amazing") alert("JS is FUN");
+// if (js === "amazing") alert("JS is FUN");
+
+let firstName = "Elango";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Typing";
+let myCurrentJob = "Full Stack Developer";
+
+console.log(firstName);
+console.log(myFirstJob);
+console.log(myCurrentJob);
