c89da88f4c8bcebe9769b64a2ee9279381e1a658
Elango <elango@Elangos-MacBook-Pro.local>
2021-03-12 00:24:57 +0530

    14_basic_operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -23,8 +23,29 @@
 // console.log(continent);
 // console.log(population);
 // console.log(typeof population);
-// ----------------------------------------------------------------------------------------
-// 13 let, const and variable
-let age = 31;
-const birthYear = 1994;
-elango = "is my name";
+// -------------------------------------------------------------------------------------
+
+// // 13 let, const and variable
+// let age = 31;
+// const birthYear = 1994;
+// elango = "is my name";
+// -------------------------------------------------------------------------------------
+
+// 14 basic operators
+// Math operators
+const now = 2021;
+const elangoBirthYear = 1994;
+const elangoAge = now - elangoBirthYear;
+console.log(elangoAge);
+
+// Assignment operators
+const fullAge = 18;
+let x = 10 + 5;
+x += 10;
+x++;
+x--;
+console.log(x);
+
+// comparison operators
+const isElangoFullAge = elangoAge >= fullAge;
+console.log(isElangoFullAge);
