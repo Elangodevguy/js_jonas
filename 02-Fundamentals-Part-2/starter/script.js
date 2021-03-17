d113afc466ab28dec002652ed43cd3dc0f688537
Elango <elango@Elangos-MacBook-Pro.local>
2021-03-18 00:12:47 +0530

    32_activating_strict_mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,10 @@
+"use strict";
+// 32 activating strict mode
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriverLicense = true;
+// if (hasDriversLicense) console.log("allowed to drive");
+
+// // const interface = 'audio'
+// // const private = true;
