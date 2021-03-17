25a87cdd530d3c3565a0d103ce4c1462039f2a9c
Elango <elango@Elangos-MacBook-Pro.local>
2021-03-17 23:00:40 +0530

    29_coding_challenge_4

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -209,7 +209,16 @@
 // }
 // ----------------------------------------------------------------
 
-// 28 ternary operator
-const age = 17;
-const drink = age >= 18 ? "wine" : "water";
-console.log(drink);
+// // 28 ternary operator
+// const age = 17;
+// const drink = age >= 18 ? "wine" : "water";
+// console.log(drink);
+// ----------------------------------------------------------------
+// 29 coding challenge 4
+const bill = 430;
+const tip = bill > 50 && bill < 300 ? bill * (15 / 100) : bill * (20 / 100);
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total bill was ${
+    bill + tip
+  }`
+);
